2f3bb864a68aa781f14a8eb8537b745868dda5bf
Anderson Martins <atmmoreira.rj@gmail.com>
2022-07-26 21:52:03 -0300

    finish project

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+const container = document.getElementById('container');
+const colors = ['#C0C0C0', '#808080', '#800000', '#808000', '#00FF00', '#00FFFF', '#008080', '#000080', '#FF00FF', '#800080', '#CD5C5C', '#F08080', '#FA8072', '#E9967A', '#FFA07A'];
+const SQUARES = 500;
+
+for (let i = 0; i < SQUARES; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+
+  square.addEventListener('mouseover', () => setColor(square));
+  square.addEventListener('mouseleave', () => removeColor(square));
+
+  container.appendChild(square);
+}
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.background = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(element) {
+  element.style.background = '#1d1d1d';
+  element.style.boxShadow = '0 0 2px #000';
+}
+
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
